2734968b8a7c40e0fdce6d8fadc4040c7265b9d8
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-09-15 14:00:04 +0300

    Example updated.

--- lecture2/lecture2_4.py
+++ lecture2/lecture2_4.py
@@ -13,4 +13,9 @@ radius = 13
 #     => 2 * pi      * radius
 border = 2 * math.pi * radius
 
+# remember to save the result of the rounding
+# back to the variable (otherwise it doesn't work)
+# e.g. just using round(border, 2) doesn't work!
+border = round(border, 2)
+
 print(f"Circumference: {border} cm")
