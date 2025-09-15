a8de7368d483be4fd73d32e273ba80903414a053
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-09-15 12:58:19 +0300

    Example updated.

--- lecture2/lecture2_2.py
+++ lecture2/lecture2_2.py
@@ -5,3 +5,8 @@ print("Some text here!\nThis goes to another line!\n\n")
 # sometimes \n is not enough, you might need also to add \r (carriage return}
 print("Something else...")
 print()
+
+# tabs can be used to align pieces of text horizontally
+print("Total:\t\t350 €")
+print("Tax:\t\t120 €")
+print("Postage:\t20 €")
