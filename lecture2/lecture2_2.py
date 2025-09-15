7768dbcb91ce8268377a88f438a91d9922fe03cd
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-09-15 12:53:48 +0300

    newline/tab examples added.

---
+++ lecture2/lecture2_2.py
@@ -0,0 +1,7 @@
+# adding new lines within text
+print("Some text here!\nThis goes to another line!\n\n")
+
+# empty print() also adds a single \n by default
+# sometimes \n is not enough, you might need also to add \r (carriage return}
+print("Something else...")
+print()
