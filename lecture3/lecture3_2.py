47013097ca5fe6150441c819b91e99aaf4406f7b
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-09-23 08:49:23 +0300

    Lecture update.

---
+++ lecture3/lecture3_2.py
@@ -0,0 +1,10 @@
+# user's age, replace this with input later
+age = 19
+
+# conditional statement -> is age under 20?
+# if it is => run the print-code below
+if age < 20:
+    print("You are less than 20 years old.")
+    print("This is also printed if less than 20 years old!")
+
+print("This line is always printed, REGARDLESS of the if-statement above.")
