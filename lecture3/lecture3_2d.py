610aeff71e7b902bcc1628b74e50e9a7c6c86a45
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-09-23 09:13:02 +0300

    Lecture update.

--- lecture3/lecture3_2d.py
+++ lecture3/lecture3_2d.py
@@ -2,6 +2,10 @@
 age = input("How old are you?\n")
 age = int(age)
 
+# ask user the month number when they would like to visit your shop
+month = input("Which month you'd like to visit the store?\n")
+month = int(month)
+
 # conditional statement -> is age under 20?
 # if it is => run the print-code below
 if age < 20:
@@ -12,5 +16,11 @@ elif age < 40:
     print("You are less than 40 years old.")
 else:
     print("You age is something else.")
+
+# since the month of the store has nothing to do
+# with the user's age, have a separate if-statement!
+if month == 7:
+    print("We're closed on July.")
+    print("Back open in August, welcome!")
 
 print("Thank you for using the application!")
