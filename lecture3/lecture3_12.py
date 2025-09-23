f7b51e27f3378baa0f6473e1396351eef6a3d524
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-09-23 10:39:22 +0300

    Lecture update.

---
+++ lecture3/lecture3_12.py
@@ -0,0 +1,13 @@
+# you could also ask these from the user
+age = 20
+city = "Rovaniemi"
+student = True
+
+# condition that is depending on three different variables
+# => "a very specific condition"
+if age >= 18 and city == "Rovaniemi" and student == True:
+    print("An adult student from Rovaniemi")
+    print("If for some reason you need to have a very specific condition")
+    print("For example, marketing / information purposes, you could do this.")
+
+print("Thank you for using our application.")
