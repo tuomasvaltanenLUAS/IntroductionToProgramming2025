9bb50a33453b1bc46969dab95ea707e63c60a4af
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-09-23 08:56:25 +0300

    Lecture update.

---
+++ lecture3/lecture3_2b.py
@@ -0,0 +1,13 @@
+# ask user's age (integer)
+age = input("How old are you?\n")
+age = int(age)
+
+# conditional statement -> is age under 20?
+# if it is => run the print-code below
+if age < 20:
+    print("You are less than 20 years old.")
+else:
+    print("You are at least 20 years old.")
+
+
+print("Thank you for using the application!")
