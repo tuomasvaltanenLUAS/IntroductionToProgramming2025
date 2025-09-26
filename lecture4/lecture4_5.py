6d2ffc160a63e2b80e6d36397e1cece42cb49de2
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-09-26 10:47:21 +0300

    Lecture update

--- lecture4/lecture4_5.py
+++ lecture4/lecture4_5.py
@@ -14,3 +14,37 @@
 # inform the user "Incorrect operation."
 
 # print the result rounded to one decimal
+
+# PHASE 1: ask the variables, convert as needed
+number1 = input("Give first number:\n")
+number1 = float(number1)
+
+number2 = input("Give second number:\n")
+number2 = float(number2)
+
+operation = input("Which operation you would like to perform? (+, -, *, /)\n")
+
+# create a variable before any conditionals, to keep track
+# of final result
+result = 0
+
+# PHASE 2: program logic, decide which operation to perform
+if operation == "+":
+    result = number1 + number2
+elif operation == "-":
+    result = number1 - number2
+elif operation == "*":
+    result = number1 * number2
+elif operation == "/":
+    # check that we don't accidentally divide by zero
+    if number2 == 0:
+        print("Can't divide by zero!")
+    else:
+        result = number1 / number2
+
+else:
+    print("Incorrect operation")
+
+# all calculations done, print the result
+result = round(result, 1)
+print(f"Result: {result}")
