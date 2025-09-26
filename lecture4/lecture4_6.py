9be1abde78497b5e4d3bf29c143ab04a33d29609
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-09-26 10:52:18 +0300

    Lecture update

---
+++ lecture4/lecture4_6.py
@@ -0,0 +1,23 @@
+# TASK 3: Create an application that asks the user the name of the product
+# and the amount of products they wish to buy.
+# for example: they want to buy 10 smartphones etc.
+
+# Also ask if the user has a discount voucher (yes/no)
+
+# the pricing is the following:
+# - smartphone = 599 € per item
+# - computer = 899 € per item
+# - coffee machine = 129 € per item
+
+# calculate the total price of the user's products
+
+# if the user has a discount voucher, reduce 15% from the total price
+
+# Finally, print the price rounded to two decimals and inform
+# the user IF a voucher was used to reduce the price.
+# If no voucher used, don't mention it.
+
+# things to think about:
+# do we need nesting?
+# do we need if-elif or separate if -structures
+# ...or do we need both?
