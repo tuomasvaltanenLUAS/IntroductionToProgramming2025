7512c11f939663398a0793131c5c91982df5ca8a
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-09-26 10:39:43 +0300

    Added file.

---
+++ lecture4/lecture4_5.py
@@ -0,0 +1,16 @@
+# TASK 2: Create a simple calculator, where user can perform
+# one of the following operations: +, -, * and /
+
+# in the beginning, ask the user for two numbers in float-format,
+# ask finally which operation they would like to perform BETWEEN the numbers
+# after the inputs, perform the selected operation between the numbers,
+# if the selections were correct
+
+# if user selects division (/), check that the second number is not 0.
+# if user still gives a 0 as a second number, INFORM THE USER
+# that we can't divide by zero
+
+# if the user selects an operation that is not supported
+# inform the user "Incorrect operation."
+
+# print the result rounded to one decimal
