533fb26111b3a96d274b59575f53ee96543f09f2
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-09-26 08:47:41 +0300

    Lecture update.

--- lecture4/lecture4_1.py
+++ lecture4/lecture4_1.py
@@ -1,3 +1,22 @@
-# lecture 4, 26.9.2025 - More conditional statements,
-# variable scope, Boolean...
-print("Welcome!")
+# our example variables, we could ask these from user too (input)
+age = 22
+city = "Helsinki"
+
+# our code should give instructions to adult users
+# based on their hometown and its health care services
+
+# UNDERAGE USES should be directed to use their own school's
+# health care services INSTEAD
+if age >= 18:
+    print("Adult instructions here!")
+
+    # at this point we can be sure that the user is at least 18 years old
+    # think these nested if-statements like a FOLLOW-UP question to the
+    # previous if-statement
+    if city == "Rovaniemi":
+        print("Health care address for adults: Test Alley 12")
+    elif city == "Helsinki":
+        print("Health care address for adults: Somewhere Road 52")
+
+else:
+    print("Underage students: contact your school's health care services!")
