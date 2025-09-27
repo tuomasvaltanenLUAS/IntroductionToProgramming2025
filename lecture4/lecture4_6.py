e2acd087b417af5623725a0fa002f6ef2ef64a08
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-09-27 17:26:20 +0300

    Example solution added.

--- lecture4/lecture4_6.py
+++ lecture4/lecture4_6.py
@@ -21,3 +21,39 @@
 # do we need nesting?
 # do we need if-elif or separate if -structures
 # ...or do we need both?
+
+# EXAMPLE SOLUTION:
+
+# PHASE 1: Ask needed variables from user
+product = input("Which product you wish to buy? (smartphone, computer, coffee machine)\n")
+amount = input("How many items of this product would you like to buy?\n")
+amount = int(amount)
+
+# voucher, yes/no
+discount = input("Do you have a discount voucher? (yes/no)\n")
+
+# initialize result variable
+total = 0
+
+# having two separate structures of if
+# if-elif-etc for price logic and if-else for voucher
+# is probably the most straight-forward approach here
+
+# PHASE 2: price logic
+if product == "smartphone":
+    total = amount * 599
+elif product == "computer":
+    total = amount * 899
+elif product == "coffee machine":
+    total = amount * 129
+else:
+    print("Unidentified product.")
+
+# voucher logic
+if discount == "yes":
+    # 15% discount => 1 - 0.15 => 0.85
+    total = total * 0.85
+
+# PHASE 3: round values and print
+total = round(total, 2)
+print(f"Total price: {total} €")
