d1bdcd41e396b24726dc1d6a880f1d8e9ffb4ac4
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-10-20 14:24:11 +0300

    Lecture update.

---
+++ lecture6/lecture6_10a.py
@@ -0,0 +1,11 @@
+print("Start!")
+
+for x in range(10):
+    # if x is equal to 6 => stop the loop
+    if x == 6:
+        print("Data found, stop searching\n")
+        break
+
+    print(x)
+
+print("End!")
---
+++ lecture6/lecture6_10b.py
@@ -0,0 +1,11 @@
+print("Start!")
+
+for x in range(10):
+    # if x is equal to 6 => skip this iteration
+    if x == 6:
+        print("Skip data, incorrect format.")
+        continue
+
+    print(x)
+
+print("End!")
