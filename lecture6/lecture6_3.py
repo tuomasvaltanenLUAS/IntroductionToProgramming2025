12febfa30c8ffd1ae09e9f016fec6a35af22f468
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-10-20 13:08:26 +0300

    Lecture update.

---
+++ lecture6/lecture6_3.py
@@ -0,0 +1,9 @@
+print("Start!")
+
+# try this code also in Python Tutor
+# in order to see how it works in practice
+for cycle in range(10):
+    number = cycle * 2
+    print(f"Number: {number}")
+
+print("End!")
