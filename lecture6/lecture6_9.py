b5e1827edca5ffd70883ceabf8dc96c0d32da886
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-10-20 14:12:18 +0300

    Lecture update.

---
+++ lecture6/lecture6_9.py
@@ -0,0 +1,20 @@
+# more practical version of nested loops
+print("Today's sales report:\n")
+
+# our logic is this =>
+# we have customer orders, each order has a certain amount of products
+
+# so, orders => products
+# we could also have even more complex stuff, like
+# e.g. department => orders => products
+
+# first, let's loop the orders
+for order in range(3):
+    print(f"Start processing order no. {order + 1}")
+
+    # process each product WITHIN this current order
+    # here we assume each order has exactly 5 orders
+    for product in range(5):
+        print(f"\t\tProcessing order no. {order + 1}, product: {product + 1} ")
+
+print("\nEnd of today's report (all orders processed).")
