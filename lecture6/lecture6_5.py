3c169c17f3cd4d81ffc66ef4b6a550c844eec4b3
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-10-20 13:18:16 +0300

    Lecture update.

---
+++ lecture6/lecture6_5.py
@@ -0,0 +1,11 @@
+print("Start!")
+
+# for loop 10 cycles
+# try also in Python Tutor
+for x in range(10):
+    if x % 2 == 0:
+        print(f"Cycle: {x} ==> (even)")
+    else:
+        print(f"Cycle: {x} ==> (odd)")
+
+print("End!")
