301b4ae33be79ae9ca7116c17ed7a1a833ddb6b0
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-10-20 13:56:50 +0300

    Lecture update.

---
+++ lecture6/lecture6_7.py
@@ -0,0 +1,20 @@
+# variable that controls if the app should be still running
+running = True
+
+# in this situation we can't know the exact amount
+# of cycles, because we never know how many times the
+# user wants to use this application
+# this is why while-loop is more natural (compared to for-loop)
+while running:
+    print("Run app! Ask stuff from user, calculate something etc...")
+    print()
+
+    # ask user if they want to continue or exit app
+    answer = input("Would you like to continue? (y/n)?\n")
+
+    # check user's answer
+    if answer.lower() == "n":
+        running = False
+
+# thank the user and end application
+print("\nThank you for using the application! (END)")
