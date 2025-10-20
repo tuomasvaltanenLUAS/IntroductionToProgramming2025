cebeab75468a68937906e3a9e551cddda8249a48
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-10-20 13:59:03 +0300

    Lecture update.

--- lecture6/lecture6_7.py
+++ lecture6/lecture6_7.py
@@ -9,6 +9,16 @@ while running:
     print("Run app! Ask stuff from user, calculate something etc...")
     print()
 
+    # here comes all the code that should be run on each cycle
+    # => ask user inputs, calculate etc.
+    number = input("Give a number:\n")
+    number = int(number)
+
+    # double the value and print it out
+    total = number * 2
+    print(f"Doubled: {total}")
+    print()
+
     # ask user if they want to continue or exit app
     answer = input("Would you like to continue? (y/n)?\n")
 
