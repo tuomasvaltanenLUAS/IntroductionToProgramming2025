81502c31cc063136c2951ca2dffdb5c5d7d7b536
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-10-20 14:34:16 +0300

    Lecture update.

---
+++ lecture6/lecture6_11.py
@@ -0,0 +1,12 @@
+# use loops to automate parts of code
+
+total = 0
+
+# use loop to ask 10 numbers from user
+# and add it total
+for x in range(10):
+    number = int(input("Give a number:\n"))
+    total = total + number
+
+print("Total sum of the numbers:")
+print(total)
