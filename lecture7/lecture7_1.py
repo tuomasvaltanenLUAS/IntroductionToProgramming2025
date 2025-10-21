00c3609310e2637899e6c0fae6ec684d8caceee7
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-10-21 08:34:30 +0300

    Lecture update.

---
+++ lecture7/lecture7_1.py
@@ -0,0 +1,16 @@
+# create a list of products
+products = ["Washing machine", "Coffee maker", "Freezer", "Fridge"]
+
+# this is the raw format of the collection if printed
+# ONLY USE for testing/debugging purposes
+# users generally don't want to see this format
+# also, it always shows everything in the collection
+# (including potentially sensitive information, if there's any)
+# print(products)
+
+# get a single element from the list
+# first index is 0, second is 1 etc.
+# kinda like -1 of what you're seeking
+# otherwise acts like a bunch of variables!
+text = products[1]
+print(text)
