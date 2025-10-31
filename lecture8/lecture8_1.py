17f8fad0ac41f7075b0b5938caa91be94779dcd6
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-10-31 09:58:38 +0200

    Lecture update.

---
+++ lecture8/lecture8_1.py
@@ -0,0 +1,7 @@
+# see installation instructions for external
+# Python modules in Moodle
+from colorama import Fore, Back, Style
+
+# change the text color
+print(Fore.CYAN + "Different text color!")
+print("Same rules for colors still apply!")
