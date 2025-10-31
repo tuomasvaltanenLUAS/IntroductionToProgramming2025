f4fb7484cb5b4ca13bf48c9273f126df743c744a
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-10-31 10:02:13 +0200

    Lecture update.

--- lecture8/lecture8_1.py
+++ lecture8/lecture8_1.py
@@ -3,5 +3,14 @@
 from colorama import Fore, Back, Style
 
 # change the text color
-print(Fore.CYAN + "Different text color!")
+print(Fore.MAGENTA + "Different text color!")
 print("Same rules for colors still apply!")
+
+# switch also the background color
+# the text color remains the same, because we didn't
+# change Foreground, only the background
+print(Back.LIGHTBLACK_EX + "Change the background too!")
+
+# reset all back to default
+print(Style.RESET_ALL)
+print("Back to normal text representation!")
