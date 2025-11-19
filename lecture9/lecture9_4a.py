5ba23665b6266b247bc2d2db408278a110c5ae5c
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-11-19 13:39:24 +0200

    Lecture update.

--- lecture9/lecture9_4a.py
+++ lecture9/lecture9_4a.py
@@ -25,26 +25,29 @@ hotel_2 = {
 # place both hotels into one list
 hotels = [hotel_1, hotel_2]
 
-#print(hotels)
-
+# just printing the basic way, very hard to see the structure
+# use a tool like var_dump instead
+# print(hotels)
 # vd.var_dump(hotels)
 
-# take just the first hotel
+# sometimes easier to just inspect one dictionary
 # first_hotel = hotels[0]
 # vd.var_dump(first_hotel)
 
+# you can copy the var_dump result as a code
+# comment => easier to refer what the structure is!
 # #0 dict(6)
-# ['name'] => str(16) "Snow Line Hotels"
-# ['rating'] => float(4.3)
-# ['wifi'] => bool(True)
-# ['free_breakfast'] => bool(True)
-# ['services'] => list(5)
-#     [0] => str(5) "sauna"
-#     [1] => str(8) "meetings"
-#     [2] => str(10) "restaurant"
-#     [3] => str(7) "parking"
-#     [4] => str(7) "safaris"
-# ['price_level'] => int(4)
+#     ['name'] => str(16) "Snow Line Hotels"
+#     ['rating'] => float(4.3)
+#     ['wifi'] => bool(True)
+#     ['free_breakfast'] => bool(True)
+#     ['services'] => list(5)
+#         [0] => str(5) "sauna"
+#         [1] => str(8) "meetings"
+#         [2] => str(10) "restaurant"
+#         [3] => str(7) "parking"
+#         [4] => str(7) "safaris"
+#     ['price_level'] => int(4)
 
 for hotel in hotels:
     print(hotel['name'])
