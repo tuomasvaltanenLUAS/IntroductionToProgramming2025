5e7b2b30b540d66c000aa95e9ca2e50c33176900
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-11-25 13:05:17 +0200

    Added a number count examples to extras.

---
+++ lecture_10/functions.py
@@ -0,0 +1,16 @@
+# functions.py, this file contains
+# the definitions of all our functions
+# kinda like a toolkit, ready to be used
+# by our other applications
+
+# NOTE: a function does NOTHING in code
+# unless it's used/called somewhere
+
+# define a function show_text()
+# this means: "what should my code do, if something HAPPENED to
+# call this function somewhere"
+def show_text():
+    print("Welcome to our application!")
+    print("---------------------------")
+    print("Please follow the instructions.")
+    print()
---
+++ lecture_10/lecture10_1.py
@@ -0,0 +1,7 @@
+# this means =>
+# from functions.py => import everything I have there
+from functions import *
+
+print("START!")
+show_text()
+print("END!")
